fa1cfebf877b87b0f8ce82134ecf2961335488c8
DevWebAma <148153050+DevWebAma@users.noreply.github.com>
2024-06-05 01:35:34 +0200

    integration template + partials

--- app/config/params.php
+++ app/config/params.php
@@ -1,6 +1,9 @@
 <?php
+//Initialisation des zones dynamiques
+$title='';
+$ontent='';
 
 define('DB_HOST', 'localhost:8889');
-define('DB_NAME', 'book_hunter');
+define('DB_NAME', 'book_hunter_2024');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
